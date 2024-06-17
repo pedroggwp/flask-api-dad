027b1b5550507c6d8208304a2f8145b1f16c33a0
pedroggwp <phsnnascimento@outlook.com>
2024-06-17 07:15:09 -0300

    subindo API novamente

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import subprocess
-
+ 
 app = Flask(__name__)
-CORS(app)  # Serve para pegar as requisições no local do js
-
+CORS(app) #Serve pra pegar as requisições no local do js
+ 
+#Definindo uma "rota" para as requisições
 @app.route('/execute', methods=['POST'])
 def execute_codigo():
+    # Pegando as informações do json
     data = request.get_json()
     codigo = data.get('codigo')
     try:
+        # Rodando o codigo pego na requisiçãop
         result = subprocess.run(
             ['python3', '-c', codigo],
-            capture_output=True, text=True
+            # pegando a saida e indicando como string
+            capture_output=True, text=True, input="Analista de dados"
         )
+        # Se o retorno for um erro, juntando o retorno com o erro
         output = result.stdout + result.stderr
     except Exception as e:
         output = f"Erro! {e}"
+    # Retornando a saida como objeto json
     return jsonify({'output': output})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
